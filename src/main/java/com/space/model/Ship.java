7bf8d9db650fa69f17c6350f6e87de45c0d506dd
r-kh <raphael@mail.ru>
2020-05-23 05:32:47 +0300

    Initial commit

---
+++ src/main/java/com/space/model/Ship.java
@@ -0,0 +1,98 @@
+package com.space.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ship")
+public class Ship {
+
+    // поля
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "planet")
+    private String planet;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "shipType")
+    private ShipType shipType;
+
+    @Column(name = "prodDate")
+    private Date prodDate;
+
+    @Column(name = "isUsed")
+    private Boolean isUsed;
+
+    @Column(name = "speed")
+    private Double speed;
+
+    @Column(name = "crewSize")
+    private Integer crewSize;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    // геттеры
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public Boolean getUsed() {
+        return isUsed;
+    }
+    public Double getSpeed() {
+        return speed;
+    }
+    public String getPlanet() {
+        return planet;
+    }
+    public Double getRating() {
+        return rating;
+    }
+    public Date getProdDate() {
+        return prodDate;
+    }
+    public Integer getCrewSize() {
+        return crewSize;
+    }
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    // сеттеры
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUsed(Boolean used) {
+        isUsed = used;
+    }
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+    public void setPlanet(String planet) {
+        this.planet = planet;
+    }
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+    public void setProdDate(Date prodDate) {
+        this.prodDate = prodDate;
+    }
+    public void setCrewSize(Integer crewSize) {
+        this.crewSize = crewSize;
+    }
+    public void setShipType(ShipType shipType) {
+        this.shipType = shipType;
+    }
+}
